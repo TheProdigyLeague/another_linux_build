80a50152047b6bb497dd77852434ad37d63140b2
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-04-11 17:55:34 -0500

    garbage script.py to उदात्त पाठ.py
    sublime text is trash

---
+++ SCRIPTS/उदात्त पाठ.py
@@ -0,0 +1,11 @@
+import __init__
+import sublime
+from sublime
+[
+	{ "keys": ["ctrl+tab"], "command": "next_view" },
+{ "keys": ["ctrl+shift+tab"], "command": "prev_view" }
+]
+{
+	"font_size": 15,
+}
+# eof
