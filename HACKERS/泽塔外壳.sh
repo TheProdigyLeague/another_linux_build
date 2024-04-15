69cbfffffbecd16444d19f3f3bc7ccbf70f53888
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-04-14 19:35:41 -0500

    Update 泽塔外壳.sh

--- HACKERS/泽塔外壳.sh
+++ HACKERS/泽塔外壳.sh
@@ -1,7 +1,7 @@
 ~/.zshrc # ui.sh
 for C:/usr/share/doc/_zsh/example.zshrc/
 
-setopt autocd              # change directory by typing its name
+setopt autocd              # change dir by typing its name
 setopt correct             # auto correct
 setopt interactivecomments # interactive mode
 setopt magicequalsubst     # enable filename expansion for args of form ‘anything=expression’
@@ -39,118 +39,101 @@ zstyle ':completion:*' completer _expand _complete
 zstyle ':completion:*' format 'Completing %d'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' list-colors ''
-zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or  char to insert%s
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 zstyle ':completion:*' rehash true
 zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
 zstyle ':completion:*' use-compctl false
 zstyle ':completion:*' verbose true
-zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+zstyle ':completion:*:kill:*' cmd 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
 
 # history.conf
 HISTFILE=~/.zsh_history
 HISTSIZE=1000
 SAVEHIST=2000
 setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
-setopt hist_ignore_dups       # ignore duplicated commands history list
-setopt hist_ignore_space      # ignore commands that start with space
-setopt hist_verify            # show command with history expansion to user before running it
-setopt share_history          # share command history data
+setopt hist_ignore_dups       # ignore duplicated cmd history list
+setopt hist_ignore_space      # ignore cmd that start with space
+setopt hist_verify            # show cmd with history expansion to user before running it
+setopt share_history          # share cmd history data
 
-# force zsh to show the complete history
+# force zsh to show complete history
 alias history="history 0"
 
-# If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:/usr/local/bin:$PATH
 
-# Path to your oh-my-zsh installation.
+# Path to end-user oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
-# Set name of the theme to load --- if set to "random", it will
-# load a random theme each time oh-my-zsh is loaded, in which case,
-# to know which specific one was loaded, run: echo $RANDOM_THEME
-# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+set theme name '&&' load if set "random"
+# load a random theme each time oh-my-zsh is loaded to know which specific one was loaded, run: 
+$ echo $RANDOM_THEME
+ref: https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 ZSH_THEME="agnoster"
-
-# Set list of themes to pick from when loading at random
-# Setting this variable when ZSH_THEME=random will cause zsh to load
-# a theme from this variable instead of looking in $ZSH/themes/
-# If set to an empty array, this variable will have no effect.
-# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
-
-# Uncomment the following line to use case-sensitive completion.
-# CASE_SENSITIVE="true"
-
-# Uncomment the following line to use hyphen-insensitive completion.
+set 'themes.lst' = {"usr pick"}; # 加载随机设置时。这个变量 zeta shell 主题将等于随机。使 Zeta Shell 从变量加载主题。
+in $ZSH/themes/
+if set = "empty array" then {var}=none
+ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell", "agnoster" )
+CASE_SENSITIVE="true"
 # Case-sensitive completion must be off. _ and - will be interchangeable.
-# HYPHEN_INSENSITIVE="true"
+HYPHEN_INSENSITIVE="true"
 
-# Uncomment one of the following lines to change the auto-update behavior
-# zstyle ':omz:update' mode disabled  # disable automatic updates
-# zstyle ':omz:update' mode auto      # update automatically without asking
-# zstyle ':omz:update' mode reminder  # just remind me to update when it's time
+_zstyle ':omz:updater' mode disabled
+_zstyle ':omz:updater' mode auto
+_zstyle ':omz:updater' mode reminder
 
-# Uncomment the following line to change how often to auto-update (in days).
-# zstyle ':omz:update' frequency 13
+# टिप्पणी हटाएं
+_zstyle ':omz:update' frequency 13
 
-# Uncomment the following line if pasting URLs and other text is messed up.
-# DISABLE_MAGIC_FUNCTIONS="true"
+# टिप्पणी हटाएं URL handler
+DISABLE_MAGIC_FUNCTIONS="true"
 
-# Uncomment the following line to disable colors in ls.
-# DISABLE_LS_COLORS="true"
+# टिप्पणी हटाएं  col_ls
+DISABLE_LS_COLORS="true"
 
-# Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
+# टिप्पणी हटाएं  terminal header
+DISABLE_AUTO_TITLE="true"
 
-# Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
+# टिप्पणी हटाएं  cmd:l auto-correct
+ENABLE_CORRECTION="true"
 
-# Uncomment the following line to display red dots whilst waiting for completion.
-# You can also set it to another string to have that shown instead of the default red dots.
-# e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
-# Caution: this setting can cause issues with multiline prompts in zsh < 5.7.1 (see #5765)
-# COMPLETION_WAITING_DOTS="true"
+# टिप्पणी हटाएं red dot
+{"string: show" | -default Red_Dot}: COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f" # causes exploits
+in zsh < 5.7.1
+COMPLETION_WAITING_DOTS="true"
 
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
+# टिप्पणी हटाएं marking untracked files
+$_VCS="dirty" {docker make repo do "status" -chck type="larg" -grep};# much, much faster.
+DISABLE_UNTRACKED_FILES_DIRTY="true"
 
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# You can set one of the optional three formats:
-# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# or set a custom format using the strftime function format specifications,
-# see 'man strftime' for details.
-# HIST_STAMPS="mm/dd/yyyy"
-
-# Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
-
-# Which plugins would you like to load?
-# Standard plugins can be found in $ZSH/plugins/
-# Custom plugins may be added to $ZSH_CUSTOM/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
-plugins=(git adb ansible rust colored-man-pages docker zsh-autosuggestions golang)
-
-source $ZSH/oh-my-zsh.sh
-
-# User configuration
-
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# enable color support of ls, less and man, and also add handy aliases
+# टिप्पणी हटाएं mod cmd ".exe" time stamp shown in history cmd output
+['END_USER']: set * -format
+$ echo "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+$ echo "custom format" strftime {"func format spec"};
+    'man strftime' for -det
+        HIST_STAMPS="mm/dd/yyyy"
+$ZSH/custom?/c=folder
+    ZSH_CUSTOM=/path/to/new-custom-folder # plugin
+...
+$ZSH/plugins/
+['+']: $ZSH_CUSTOM/plugins/
+    plugins=ruby_on_rails -git '.txt mate' LIGHTHOUSE
+        ...
+$ echo            STARTUP .sh
+> plugins= -git adb_ansible | rust_col -man docker .zsh-autosuggestion -golang
+src=$ZSH/oh-my-zsh.sh
+    usr.conf
+        [err]
+export MANPATH="/usr/local/man:$MANPATH"
+$ -man lang env
+    export LANG=en_US.UTF-8
+$ sublime.exe
+    localHOST: 8080
+        RAT.x if [[ -n $SSH_CONNECTION ]]; then
+            export EDITOR='vim'
+                else
+            export EDITOR='mvim'
+            fi # col supp ls < man + alias
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     export LS_COLORS="$LS_COLORS:ow=30;44:" # fix ls color for folders with 777 permissions
@@ -173,29 +156,19 @@ if [ -x /usr/bin/dircolors ]; then
     export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
     export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
 
-    # Take advantage of $LS_COLORS for completion as well
+$LS_COLORS for تحديث النسخة الكاملة
     zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
     zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
 fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
-
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
-## ADDED
-
-### Commands
-setxkbmap fr
-neofetch
-
-### Alias
+$ flag compiler then export ARCHFLAGS="-arch x86_64"
+    set alias -override
+        oh-my-zsh/libs/plugin/theme
+$ define alias "ZSH_CUSTOM"
+    `alias.exe`
+    alias zshconfig="mate ~/.zshrc"
+    alias ohmyzsh="mate ~/.oh-my-zsh"
+[+]: setxkbmap fr
+    neofetch.cmd
 alias ap='sudo apt update && sudo apt upgrade -y'
 alias cd..='cd ..'
 alias grep='grep --color=auto'
@@ -215,19 +188,11 @@ alias get-meteo='curl https://wttr.in/'
 alias cpy='xclip -selection clipboard'
 alias b64d='base64 -d'
 alias b64e='base64 -w 0'
-
-## Functions
+function
 mkcd()
-{
-  mkdir -p -- "$1" && cd -P -- "$1"
-}
-
+{$ mkdir -p -- "$1" && cd -P -- "$1"}
 wordlists()
-{
-find -L /usr/share/wordlists /opt/SecLists/ -type f -iname "*$1*"
-}
-
-## PATH export
+{find -L /usr/share/wordlists /opt/SecLists/ -type f -iname "*$1*"}
 export PATH="$PATH:$HOME/.local/bin:/usr/local/go/bin:$HOME/.cargo/bin:$HOME/go/bin"
-
 eval "$(atuin init zsh --disable-up-arrow)"
+#eof
